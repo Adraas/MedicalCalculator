59b6e44a482dfb256eebc4d7263d97207c71bd27
Anton Fomichev <fomichev34591@mail.ru>
2019-07-02 19:41:20 +0400

    feat(server side): add DAO

---
+++ repository/src/main/java/dao/IDAO.java
@@ -0,0 +1,21 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IDAO<Entity> {
+
+    /**create*/
+    void add(Entity entity) throws SQLException;
+
+    /**read*/
+    List<Entity> getAll() throws SQLException;
+
+    Entity getById(String id) throws SQLException;
+
+    /**update*/
+    void update(Entity entity) throws SQLException;
+
+    /**delete*/
+    void remove(Entity entity) throws SQLException;
+}
---
+++ repository/src/main/java/dao/QuestionaryDAO.java
@@ -0,0 +1,31 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public class QuestionaryDAO extends DAO{
+    @Override
+    public void add(Object o) throws SQLException {
+
+    }
+
+    @Override
+    public List getAll() throws SQLException {
+        return null;
+    }
+
+    @Override
+    public Object getById(String id) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void update(Object o) throws SQLException {
+
+    }
+
+    @Override
+    public void remove(Object o) throws SQLException {
+
+    }
+}
