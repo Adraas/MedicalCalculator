83fafe4e00626a52062794c5076807972bc94388
Adraas <pickalov.artyom@yandex.ru>
2019-07-03 13:57:10 +0400

    refactor(server-side): update DAO The comments was deleted, the parameter was added into method getAll.

--- repository/src/main/java/ru/code/open/dao/IDao.java
+++ repository/src/main/java/ru/code/open/dao/IDao.java
@@ -4,17 +4,13 @@ import java.util.List;
 
 public interface IDao<Entity> {
 
-    //create
     void add(Entity entity);
-
-    //read
-    List<Entity> getAll();
 
     Entity getById(String id);
 
-    //update
     void update(Entity entity);
 
-    //delete
     void remove(Entity entity);
+
+    List<Entity> getAll(String tableName);
 }
