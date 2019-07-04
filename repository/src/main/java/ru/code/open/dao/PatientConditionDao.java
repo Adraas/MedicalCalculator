a92ddc41121aab01af0ffcd096908ae0ba81da3d
Anton Fomichev <fomichev34591@mail.ru>
2019-07-04 11:10:41 +0400

    feat(server side): modify DAO level

--- repository/src/main/java/ru/code/open/dao/PatientConditionDao.java
+++ repository/src/main/java/ru/code/open/dao/PatientConditionDao.java
@@ -3,8 +3,4 @@ package ru.code.open.dao;
 import ru.code.open.entities.PatientCondition;
 
 public class PatientConditionDao extends Dao<PatientCondition> {
-    @Override
-    public PatientCondition getById(String id) {
-        return util.HibernateUtil.getSessionFactory().openSession().get(PatientCondition.class, id);
-    }
 }
--- repository/src/main/java/ru/code/open/dao/QuestionnaireDao.java
+++ repository/src/main/java/ru/code/open/dao/QuestionnaireDao.java
@@ -3,8 +3,4 @@ package ru.code.open.dao;
 import ru.code.open.entities.Questionnaire;
 
 public class QuestionnaireDao extends Dao<Questionnaire> {
-    @Override
-    public Questionnaire getById(String id) {
-        return util.HibernateUtil.getSessionFactory().openSession().get(Questionnaire.class, id);
-    }
 }
