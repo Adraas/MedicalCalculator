c885a4aff1aa6bf054e31b376c3594c8f04c8e67
RenataKst <36596693+renatakst@users.noreply.github.com>
2019-07-06 15:36:36 +0400

    refactor(entity): adding new features into entities

--- repository/src/main/java/ru/code/open/entities/Patient.java
+++ repository/src/main/java/ru/code/open/entities/Patient.java
@@ -1,5 +1,48 @@
 package ru.code.open.entities;
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.HashSet;
+import java.util.Set;
 
+@Entity
+@Table(name="patient")
 public class Patient {
-    // TODO: add attributes with ORM configuration
+    public Patient () {}
+
+
+    @Column(name = "patientId")
+    private Long id;
+    @Column(name = "patientName")
+    private String name;
+    @Column(name = "patientSurname")
+    private String surname;
+    @Column(name = "patientPatronymic")
+    private String patronymic;
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @OneToMany(mappedBy = "patient", cascade=CascadeType.ALL, orphanRemoval=true)
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name;}
+
+    public void setName(String name) { this.name = name; }
+
+    public String getSurname() { return surname; }
+
+    public void setSurname(String surname) { this.surname = surname; }
+
+    public String getPatronymic() { return patronymic; }
+
+    public void setPatronymic(String patronymic) { this.patronymic = patronymic; }
+
+    @Override
+    public String toString(){
+        return "Patinent{ " + "id = " + id + ", name = '" + name + '\'' + ", surname = '" + surname + '\'' + ", patronymic = '" + patronymic + '\'' + '}';
+    }
 }
--- repository/src/main/java/ru/code/open/entities/PatientCondition.java
+++ repository/src/main/java/ru/code/open/entities/PatientCondition.java
@@ -1,5 +1,38 @@
 package ru.code.open.entities;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name="pacientcondition")
 public class PatientCondition {
-    // TODO: add attributes with ORM configuration
+    public PatientCondition () {}
+
+
+    @Column(name = "patientId")
+    private long id;
+    @Column(name = "patientCondition")
+    private String condition;
+    @Column(name = "Description")
+    private String descriprion;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @ManyToOne
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getCondition() { return condition;}
+
+    public void setCondition(String condition) { this.condition = condition; }
+
+    public String getDescriprion() { return descriprion;}
+
+    public void setDescriprion(String descriprion) { this.descriprion = descriprion; }
+
+    @Override
+    public String toString(){
+        return "PatinentCondition{ " + "id = " + id + ", condition = '" + condition + '\'' + ", description = '" + descriprion + '\'' + '}';
+    }
+
 }
