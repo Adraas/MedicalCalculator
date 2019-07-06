e3c282bd42f6128ce0b68ca31b57106ddbe2ba0f
RenataKst <36596693+renatakst@users.noreply.github.com>
2019-07-06 15:36:36 +0400

    refactor(entity): adding new features into entities

--- repository/src/main/java/ru/code/open/entities/Questionnaire.java
+++ repository/src/main/java/ru/code/open/entities/Questionnaire.java
@@ -1,5 +1,42 @@
 package ru.code.open.entities;
 
+import ru.code.open.embeddables.Question;
+
+import javax.persistence.Entity;
+import javax.persistence.*;
+import javax.persistence.Table;
+import java.util.Collection;
+
+@Entity
+@Table(name="questionnaire")
 public class Questionnaire {
-    // TODO: add attributes with ORM configuration
+    public Questionnaire () {}
+
+
+    @Column(name = "patientId")
+    private long id;
+    @Column(name = "Title")
+    private String title;
+    @Column(name = "Question")
+    private Collection <Question> questions;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    public Long getId() { return id; }
+
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitle() { return title;}
+
+    public void setTitle(String title) { this.title = title; }
+
+    private Collection<Question> getQuestions() { return questions;}
+
+    public void setQuestions(Collection<Question> questions) { this.questions = questions; }
+
+    @Override
+    public String toString(){
+        return "Questionnaire{ " + "id = " + id + ", title = '" + title + '\'' + ", question = '" + getQuestions() + '\'' + '}';
+    }
+
 }
