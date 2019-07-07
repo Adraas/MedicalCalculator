289e65cb7dcb9e0df40b83e3ee3f74deaa7dd180
Adraas <pickalov.artyom@yandex.ru>
2019-07-07 16:01:46 +0400

    feat: create function initializer without implementation

---
+++ medical-calculator-model/src/main/java/ru/code/open/functions/FunctionInitializer.java
@@ -0,0 +1,5 @@
+package ru.code.open.functions;
+
+public class FunctionInitializer {
+    // TODO: add initialize for all functions from the our collection and collect them to the FunctionRepository class
+}
