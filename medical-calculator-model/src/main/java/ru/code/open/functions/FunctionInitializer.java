f7062a8f81819d0534d7dbfeec515094b7bde36e
Adraas <pickalov.artyom@yandex.ru>
2019-07-08 18:02:34 +0400

    feat: add subs for all function implementations

--- medical-calculator-model/src/main/java/ru/code/open/functions/FunctionInitializer.java
+++ medical-calculator-model/src/main/java/ru/code/open/functions/FunctionInitializer.java
@@ -1,5 +1,40 @@
 package ru.code.open.functions;
 
+import java.util.Map;
+import java.util.function.Function;
+
+// TODO: implement Function objects initializing methods
 public class FunctionInitializer {
-    // TODO: add initialize for all functions from the our collection and collect them to the FunctionRepository class
+
+    public static void initializeFunctions() {
+        FunctionRepository.addFunctionForGivenCalculator("Оценка клиренса креатинина по Cockcroft - Gault",
+                creatinineClearanceFunctionInit());
+        FunctionRepository.addFunctionForGivenCalculator("Шкала GRACE", measureGRACEFunctionInit());
+        FunctionRepository.addFunctionForGivenCalculator("Скорость клубочковой фильтрации по формулам "
+                .concat("MDRD и Schwartz"), explorationMDRDFunctionInit());
+        FunctionRepository.addFunctionForGivenCalculator("Скорость клубочковой фильтрации по формулам "
+                .concat("MDRD и Schwartz"), measureBySchwartzGJFunctionInit());
+        FunctionRepository.addFunctionForGivenCalculator("Расчет скорости внутривенного капельного "
+                .concat("введения препарата"), drugAdministrationRateFunctionInit());
+    }
+
+    private static Function<Map<String, Double>, Double> creatinineClearanceFunctionInit() {
+        return (parametersByNames) -> null;
+    }
+
+    private static Function<Map<String, Double>, Double> measureGRACEFunctionInit() {
+        return (parametersByNames) -> null;
+    }
+
+    private static Function<Map<String, Double>, Double> explorationMDRDFunctionInit() {
+        return (parametersByNames) -> null;
+    }
+
+    private static Function<Map<String, Double>, Double> measureBySchwartzGJFunctionInit() {
+        return (parametersByNames) -> null;
+    }
+
+    private static Function<Map<String, Double>, Double> drugAdministrationRateFunctionInit() {
+        return (parametersByNames) -> null;
+    }
 }
