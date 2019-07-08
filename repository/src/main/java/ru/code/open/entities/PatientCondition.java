5027550e7f75c79c74a55d8ff55150f7a79cedc8
RenataKst <36596693+renatakst@users.noreply.github.com>
2019-07-08 15:25:48 +0400

    feat: update entities ORM

--- repository/src/main/java/ru/code/open/entities/PatientCondition.java
+++ repository/src/main/java/ru/code/open/entities/PatientCondition.java
@@ -1,38 +1,21 @@
 package ru.code.open.entities;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
+@Data
 @Entity
-@Table(name="pacientcondition")
+@Table(name = "patient_condition")
 public class PatientCondition {
-    public PatientCondition () {}
-
-
-    @Column(name = "patientId")
-    private long id;
-    @Column(name = "patientCondition")
-    private String condition;
-    @Column(name = "Description")
-    private String descriprion;
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
-    @ManyToOne
-    public Long getId() { return id; }
-
-    public void setId(Long id) { this.id = id; }
-
-    public String getCondition() { return condition;}
-
-    public void setCondition(String condition) { this.condition = condition; }
-
-    public String getDescriprion() { return descriprion;}
-
-    public void setDescriprion(String descriprion) { this.descriprion = descriprion; }
-
-    @Override
-    public String toString(){
-        return "PatinentCondition{ " + "id = " + id + ", condition = '" + condition + '\'' + ", description = '" + descriprion + '\'' + '}';
-    }
+    @Column(name = "patient_id", unique = true)
+    private long id;
+    @Column(name = "patient_condition", nullable = false)
+    private String condition;
+    @Column(name = "description", nullable = false)
+    private String descriprion;
 
 }
