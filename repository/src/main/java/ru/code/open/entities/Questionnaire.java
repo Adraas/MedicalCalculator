f17672c27f6d85832b1694e753831fbd0fd21527
Adraas <pickalov.artyom@yandex.ru>
2019-07-08 15:35:04 +0400

    style: delete unnecessary imports

--- repository/src/main/java/ru/code/open/entities/Questionnaire.java
+++ repository/src/main/java/ru/code/open/entities/Questionnaire.java
@@ -3,8 +3,13 @@ package ru.code.open.entities;
 import lombok.Data;
 import ru.code.open.embeddables.Question;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.*;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import java.util.Collection;
 
