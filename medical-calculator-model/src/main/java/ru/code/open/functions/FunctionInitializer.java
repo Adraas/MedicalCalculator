82c7da76a4abda62789621335db872490c9b236d
Adraas <pickalov.artyom@yandex.ru>
2019-07-09 14:09:19 +0400

    feat: implement missing method

--- medical-calculator-model/src/main/java/ru/code/open/functions/FunctionInitializer.java
+++ medical-calculator-model/src/main/java/ru/code/open/functions/FunctionInitializer.java
@@ -1,5 +1,8 @@
 package ru.code.open.functions;
 
+import ru.code.open.functions.util.Interval;
+import ru.code.open.functions.util.MeasureGRACEFunctionUtil;
+
 import java.util.Map;
 import java.util.function.Function;
 
@@ -27,9 +30,43 @@ public class FunctionInitializer {
         };
     }
 
-    // TODO: implement this method
     private static Function<Map<String, Double>, Double> measureGRACEFunctionInit() {
-        return (parametersByNames) -> null;
+        return (parametersByNames) -> {
+            Map<Interval<Integer>, Integer> age = MeasureGRACEFunctionUtil.getAge();
+            Map<Interval<Integer>, Integer> heartRate = MeasureGRACEFunctionUtil.getHeartRate();
+            Map<Interval<Integer>, Integer> systolicBloodPressure = MeasureGRACEFunctionUtil.getSystolicBloodPressure();
+            Map<Interval<Double>, Integer> serumCreatinine = MeasureGRACEFunctionUtil.getSerumCreatinine();
+            Map<String, Integer> heartFailureSeverity = MeasureGRACEFunctionUtil.getHeartFailureSeverity();
+            int ageScore = parametersByNames.get("Возраст").intValue();
+            ageScore = age.get(MeasureGRACEFunctionUtil.getInterval(ageScore, age));
+            int heartRateScore = parametersByNames.get("Частота сердечных сокращений").intValue();
+            heartRateScore = heartRate.get(MeasureGRACEFunctionUtil.getInterval(heartRateScore, age));
+            int systolicBloodPressureScore = parametersByNames.get("Систолическое артериальное давление").intValue();
+            systolicBloodPressureScore = systolicBloodPressure.get(MeasureGRACEFunctionUtil
+                    .getInterval(systolicBloodPressureScore, age));
+            int serumCreatinineScore = parametersByNames.get("Креатинин сыворотки крови").intValue();
+            serumCreatinineScore = serumCreatinine.get(MeasureGRACEFunctionUtil.getInterval(serumCreatinineScore, age));
+            byte heartFailureSeverityType = parametersByNames
+                    .get("Тяжесть сердечной недостаточности по классификации Killip").byteValue();
+            int heartFailureSeverityScore = heartFailureSeverityType == 0
+                    ? heartFailureSeverity.get("Кардиогенный шок (IV)")
+                    : heartFailureSeverityType == 1
+                    ? heartFailureSeverity.get("Острый отек легких (III)")
+                    : heartFailureSeverityType == 2
+                    ? heartFailureSeverity
+                    .get("Наличие хрипов в легких и/или повышенного давления в югулярных венах (II)")
+                    : heartFailureSeverity.get("Отсутствие признаков застойной сердечной недостаточности (I)");
+            byte cardiacArrest = (byte) (parametersByNames.get("Остановка сердца (на момент поступления пациента)")
+                    .byteValue() == 0 ? 39 : 0);
+            byte stSegmentDeviation = (byte) (parametersByNames.get("Отклонение сегмента ST")
+                    .byteValue() == 0 ? 28 : 0);
+            byte cardiospecificEnzymesLevelIncrease = (byte) (parametersByNames
+                    .get("Наличие диагностически значимого повышения уровня кардиоспецифических ферментов")
+                    .byteValue() == 0 ? 14 : 0);
+            return (double) (ageScore + heartRateScore + systolicBloodPressureScore + serumCreatinineScore
+                    + heartFailureSeverityScore + cardiacArrest + stSegmentDeviation
+                    + cardiospecificEnzymesLevelIncrease);
+        };
     }
 
     private static Function<Map<String, Double>, Double> explorationMDRDFunctionInit() {
