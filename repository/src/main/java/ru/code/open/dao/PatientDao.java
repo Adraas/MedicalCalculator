a388f26d6a12d5ebf7fa3d60a9f615e6869d0d84
Anton Fomichev <fomichev34591@mail.ru>
2019-07-09 15:06:14 +0400

    feat(server side): add method getPatient()

--- repository/src/main/java/ru/code/open/dao/PatientDao.java
+++ repository/src/main/java/ru/code/open/dao/PatientDao.java
@@ -1,11 +1,21 @@
 package ru.code.open.dao;
 
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import ru.code.open.entities.Patient;
 
 public class PatientDao extends Dao<Patient, Long> {
 
     public PatientDao(Class<Patient> entity, Session session) {
         super(entity, session);
+    }
+
+    public boolean getPatient(String firstName,String surname, String patronymic) {
+        NativeQuery<Patient> nativeQuery = getSession().createNativeQuery("SELECT * FROM patient WHERE patient_name :firstName AND patient_surname :surname AND patient_patronymic :patronymic");
+        nativeQuery.setParameter("firstName", firstName);
+        nativeQuery.setParameter("surname", surname);
+        nativeQuery.setParameter("patronymic", patronymic);
+        Patient patient = nativeQuery.getSingleResult();
+        return patient != null;
     }
 }
