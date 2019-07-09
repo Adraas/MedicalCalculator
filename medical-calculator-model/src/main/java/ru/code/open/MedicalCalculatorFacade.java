8f3dda479df28e0e3d64577c7aed0397c42a9967
Adraas <pickalov.artyom@yandex.ru>
2019-07-09 16:52:59 +0400

    feat: implement module facade

--- medical-calculator-model/src/main/java/ru/code/open/MedicalCalculatorFacade.java
+++ medical-calculator-model/src/main/java/ru/code/open/MedicalCalculatorFacade.java
@@ -1,4 +1,16 @@
 package ru.code.open;
 
+import lombok.Getter;
+import ru.code.open.algorithm.MedicalCalculator;
+import ru.code.open.functions.FunctionInitializer;
+
+@Getter
 public class MedicalCalculatorFacade {
+
+    private MedicalCalculator medicalCalculator;
+
+    public MedicalCalculatorFacade() {
+        medicalCalculator = new MedicalCalculator();
+        FunctionInitializer.initializeFunctions();
+    }
 }
