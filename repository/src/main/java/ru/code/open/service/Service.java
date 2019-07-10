70a25c8ac99fc8a7be71556e889d85502e641cf7
Adraas <pickalov.artyom@yandex.ru>
2019-07-10 15:26:49 +0400

    feat: add implementation The class now is abstract and with the implementation.

--- repository/src/main/java/ru/code/open/service/Service.java
+++ repository/src/main/java/ru/code/open/service/Service.java
@@ -3,7 +3,7 @@ package ru.code.open.service;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import org.hibernate.Session;
+import ru.code.open.dao.IDao;
 import ru.code.open.exceptions.PersistenceException;
 
 import java.io.Serializable;
@@ -11,33 +11,32 @@ import java.util.Collection;
 
 @AllArgsConstructor
 @Getter(value = AccessLevel.PROTECTED)
-public class Service<V, I extends Serializable> implements IService<V, I> {
+public abstract class Service<V, I extends Serializable> implements IService<V, I> {
 
-    private Class<V> entity;
-    private Session session;
+    private IDao<V, I> dao;
 
     @Override
     public void create(V newInstance) throws PersistenceException {
-
+        dao.add(newInstance);
     }
 
     @Override
     public V read(I index) {
-        return null;
+        return dao.getById(index);
     }
 
     @Override
     public void update(V transientInstance) throws PersistenceException {
-
+        dao.update(transientInstance);
     }
 
     @Override
     public void delete(V transientInstance) throws PersistenceException {
-
+        dao.remove(transientInstance);
     }
 
     @Override
     public Collection<V> getAll() {
-        return null;
+        return dao.getAll(dao.getTableName());
     }
 }
