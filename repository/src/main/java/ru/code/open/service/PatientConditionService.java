d1b3174e78bd78b0e518352884b9facf4b15bcd0
Adraas <pickalov.artyom@yandex.ru>
2019-07-10 15:27:56 +0400

    fix: fix wrong constructors

--- repository/src/main/java/ru/code/open/service/PatientConditionService.java
+++ repository/src/main/java/ru/code/open/service/PatientConditionService.java
@@ -1,11 +1,11 @@
 package ru.code.open.service;
 
-import org.hibernate.Session;
+import ru.code.open.dao.IDao;
 import ru.code.open.entities.PatientCondition;
 
 public class PatientConditionService extends Service<PatientCondition, Long> {
 
-    public PatientConditionService(Class<PatientCondition> v, Session session) {
-        super(v, session);
+    public PatientConditionService(IDao<PatientCondition, Long> dao) {
+        super(dao);
     }
 }
