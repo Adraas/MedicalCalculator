1c1e74cfd2a49eaf8b21ce32123573ef36a50fbb
Anton Fomichev <fomichev34591@mail.ru>
2019-07-10 14:26:41 +0400

    feat(server side): add method getByTitle() to QuestionnaireService

--- repository/src/main/java/ru/code/open/service/QuestionnaireService.java
+++ repository/src/main/java/ru/code/open/service/QuestionnaireService.java
@@ -1,11 +1,15 @@
 package ru.code.open.service;
 
+import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
 import ru.code.open.entities.Questionnaire;
 import ru.code.open.exceptions.PersistenceException;
 
 import java.util.Collection;
 
 public class QuestionnaireService implements IService<Questionnaire, Long> {
+
+    private Session session;
 
     @Override
     public void create(Questionnaire newInstance) throws PersistenceException {
@@ -27,5 +31,11 @@ public class QuestionnaireService implements IService<Questionnaire, Long> {
     @Override
     public Collection<Questionnaire> getAll() {
         return null;
+    }
+
+    public Questionnaire getByTitle(String title) {
+        NativeQuery<Questionnaire> nativeQuery = session.createNativeQuery("SELECT * FROM questionnaire WHERE title = :title");
+        nativeQuery.setParameter("title", title);
+        return nativeQuery.getSingleResult();
     }
 }
