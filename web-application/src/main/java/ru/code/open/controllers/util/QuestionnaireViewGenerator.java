4c4e62b675ebc8868bb1c5bad0889bc0acfac760
Adraas <pickalov.artyom@yandex.ru>
2019-07-10 14:47:33 +0400

    style: reformat result DOM String element

--- web-application/src/main/java/ru/code/open/controllers/util/QuestionnaireViewGenerator.java
+++ web-application/src/main/java/ru/code/open/controllers/util/QuestionnaireViewGenerator.java
@@ -14,18 +14,21 @@ public class QuestionnaireViewGenerator {
                 new MedicalCalculatorFacade(new RepositoryFacade("h2_entity_manager", Questionnaire.class));
         Questionnaire questionnaire =
                 ((QuestionnaireService) medicalCalculatorFacade.getRepositoryFacade().getService()).getByTitle(title);
-        String questionnaireData = "";
+        String questionnaireData = "<div>";
         for (Question question : questionnaire.getQuestions()) {
-            questionnaireData = questionnaireData.concat(question.getQuestionWording()).concat("<br/><br/>");
+            questionnaireData = questionnaireData.concat(question.getQuestionWording()).concat("<br/>");
             if (question.getAnswers().isEmpty())
-                questionnaireData = questionnaireData.concat("<input  type='text' name='answer'  value='' /><br/><br/>");
+                questionnaireData = questionnaireData.concat("<input type='text' name='answer' value=''/><br/><br/>");
             else {
                 for (Answer answer : question.getAnswers()) {
-                    questionnaireData = questionnaireData.concat(" <input  type='radio' name='answer'/>").concat(answer.getAnswerWording()).concat("<br/><br/>");
+                    questionnaireData = questionnaireData.concat("<input type='radio' name='answer'/>")
+                            .concat(answer.getAnswerWording()).concat("<br/>");
                 }
+                questionnaireData = questionnaireData.concat("<br/>");
             }
         }
-        questionnaireData = questionnaireData.concat("<input type='submit' name='button' value='Вычислить'>");
+        questionnaireData = questionnaireData.concat("<input type='submit' name='calculationButton' value='Вычислить'>")
+                .concat("</div>");
         return questionnaireData;
     }
 }
