56bec335432715bcb503bc05686669b66042a13e
Anton Fomichev <fomichev34591@mail.ru>
2019-07-10 14:40:31 +0400

    feat(server side): modify service structure

--- repository/src/main/java/ru/code/open/service/PatientService.java
+++ repository/src/main/java/ru/code/open/service/PatientService.java
@@ -1,31 +1,11 @@
 package ru.code.open.service;
 
+import org.hibernate.Session;
 import ru.code.open.entities.Patient;
-import ru.code.open.exceptions.PersistenceException;
 
-import java.util.Collection;
+public class PatientService extends Service<Patient, Long> {
 
-public class PatientService implements IService<Patient, Long> {
-
-    @Override
-    public void create(Patient newInstance) throws PersistenceException {
-    }
-
-    @Override
-    public Patient read(Long index) {
-        return null;
-    }
-
-    @Override
-    public void update(Patient transientInstance) throws PersistenceException {
-    }
-
-    @Override
-    public void delete(Patient transientInstance) throws PersistenceException {
-    }
-
-    @Override
-    public Collection<Patient> getAll() {
-        return null;
+    public PatientService(Class<Patient> v, Session session) {
+        super(v, session);
     }
 }
