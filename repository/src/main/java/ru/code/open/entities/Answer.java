b61e74e30b4155384eff4e94583d86419d3d1167
Adraas <pickalov.artyom@yandex.ru>
2019-07-10 20:01:09 +0400

    refactor: change field type

--- repository/src/main/java/ru/code/open/entities/Answer.java
+++ repository/src/main/java/ru/code/open/entities/Answer.java
@@ -4,11 +4,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Embeddable;
-import javax.persistence.FetchType;
-import javax.persistence.MapKeyColumn;
-import java.util.Map;
 
 @Data
 @NoArgsConstructor
@@ -18,12 +14,10 @@ public class Answer {
     @Column(name = "answer", nullable = false, length = 100)
     private String answerWording;
     @Column(name = "grade", nullable = false)
-    @MapKeyColumn(name = "criteria", unique = true, length = 20)
-    @ElementCollection(fetch = FetchType.EAGER, targetClass = String.class)
-    private Map<String, Long> grades;
+    private long grade;
 
-    public Answer(String answerWording, Map<String, Long> grades) {
+    public Answer(String answerWording, long grade) {
         this.answerWording = answerWording;
-        this.grades = grades;
+        this.grade = grade;
     }
 }
