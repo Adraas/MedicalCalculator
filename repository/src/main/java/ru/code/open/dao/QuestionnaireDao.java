141ee3ac7c1b88a7459d1e3eba8875903e118b52
Anton Fomichev <fomichev34591@mail.ru>
2019-07-10 13:45:23 +0400

    feat(server side): add method getByTitle()

--- repository/src/main/java/ru/code/open/dao/QuestionnaireDao.java
+++ repository/src/main/java/ru/code/open/dao/QuestionnaireDao.java
@@ -1,11 +1,20 @@
 package ru.code.open.dao;
 
 import org.hibernate.Session;
+import org.hibernate.query.NativeQuery;
+import ru.code.open.entities.Patient;
 import ru.code.open.entities.Questionnaire;
 
 public class QuestionnaireDao extends Dao<Questionnaire, Long> {
 
     public QuestionnaireDao(Class<Questionnaire> entity, Session session) {
         super(entity, session);
+    }
+
+    @SuppressWarnings(value = {"unchecked"})
+    public Questionnaire getByTitle(String title) {
+        NativeQuery<Questionnaire> nativeQuery = getSession().createNativeQuery("SELECT * FROM questionnaire WHERE tittle = :title");
+        nativeQuery.setParameter("title", title);
+        return nativeQuery.getSingleResult();
     }
 }
