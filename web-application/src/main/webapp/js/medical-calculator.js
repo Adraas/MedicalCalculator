1cb60ae78322178d3ca0e103456e72ec8c31105c
Adraas <pickalov.artyom@yandex.ru>
2019-07-11 12:57:55 +0400

    refactor: update scripts The scripts was simplified and fixed.

--- web-application/src/main/webapp/js/medical-calculator.js
+++ web-application/src/main/webapp/js/medical-calculator.js
@@ -2,40 +2,38 @@ class MedicalCalculator {
 
     static xmlHttp = new XMLHttpRequest();
 
-    static receiveQuestionnaires(parentNodeId) {
+    static receiveQuestionnaires() {
         this.xmlHttp.onload = function () {
-            let response = MedicalCalculator.xmlHttp.responseText;
-            if (response != null && response.trim() !== "") {
-                let container = document.getElementById(parentNodeId);
-                while (container.firstChild) {
-                    container.removeChild(container.firstChild);
-                }
-                let parser = new DOMParser();
-                let elementDOM = parser.parseFromString(response, "text/html");
-                document.getElementById(parentNodeId).appendChild(elementDOM);
-            }
+            let parentNodeId = "questionnaires";
+            MedicalCalculator.updateChildElements(parentNodeId);
         };
         this.xmlHttp.open("GET", "/questionnaires-loading", false);
         this.xmlHttp.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
         this.xmlHttp.send("message-type=get-all-questionnaires");
     }
 
-    static receiveQuestionnaire(parentNodeId, anchorId, questionnaireTitle) {
+    static receiveQuestionnaire(questionnaireTitle) {
         this.xmlHttp.onload = function () {
-            let response = MedicalCalculator.xmlHttp.responseText;
-            if (response != null && response.trim() !== "") {
-                let container = document.getElementById(parentNodeId);
-                while (container.firstChild) {
-                    container.removeChild(container.firstChild);
-                }
-                let parser = new DOMParser();
-                let elementDOM = parser.parseFromString(response, "text/html");
-                document.getElementById(parentNodeId).appendChild(elementDOM);
-                window.location = "#" + anchorId
-            }
+            let parentNodeId = "calculator";
+            let anchorId = "current_calculator";
+            MedicalCalculator.updateChildElements(parentNodeId);
+            window.location = "#" + anchorId
         };
         this.xmlHttp.open("GET", "/questionnaire-loading", true);
         this.xmlHttp.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
         this.xmlHttp.send("message-type=get-questionnaire-data&questionnaire-title=" + questionnaireTitle);
+    }
+
+    static updateChildElements(parentNodeId) {
+        let response = MedicalCalculator.xmlHttp.responseText;
+        if (response != null && response.trim() !== "") {
+            let container = document.getElementById(parentNodeId);
+            while (container.firstChild) {
+                container.removeChild(container.firstChild);
+            }
+            let parser = new DOMParser();
+            let elementDOM = parser.parseFromString(response, "text/html");
+            document.getElementById(parentNodeId).appendChild(elementDOM);
+        }
     }
 }
