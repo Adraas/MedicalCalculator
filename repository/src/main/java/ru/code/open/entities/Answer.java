e3c2f930fcf96eb5486956cbc256ba83692d6723
Adraas <pickalov.artyom@yandex.ru>
2019-07-11 09:14:02 +0400

    docs: add documentation for the entities

--- repository/src/main/java/ru/code/open/entities/Answer.java
+++ repository/src/main/java/ru/code/open/entities/Answer.java
@@ -6,16 +6,34 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
+/**
+ * The class {@code Answer} is an embeddable type for the {@code Question} embeddable type. This embeddable type
+ * represents a some answer data.
+ */
 @Data
 @NoArgsConstructor
 @Embeddable
 public class Answer {
 
+    /**
+     * The answer wording as a simple {@code String} object.
+     */
     @Column(name = "answer", nullable = false, length = 100)
     private String answerWording;
+
+    /**
+     * The grade of this answer.
+     */
     @Column(name = "grade", nullable = false)
     private long grade;
 
+    /**
+     * Initializes a newly created {@code Answer} object, with the initialization of the fields with the given
+     * values.
+     *
+     * @param answerWording {@link #answerWording}
+     * @param grade         {@link #grade}
+     */
     public Answer(String answerWording, long grade) {
         this.answerWording = answerWording;
         this.grade = grade;
--- repository/src/main/java/ru/code/open/entities/Question.java
+++ repository/src/main/java/ru/code/open/entities/Question.java
@@ -9,16 +9,34 @@ import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import java.util.Set;
 
+/**
+ * The class {@code Question} is an embeddable type for the {@code Questionnaire} entity. This embeddable type
+ * represents a some question data.
+ */
 @Data
 @NoArgsConstructor
 @Embeddable
 public class Question {
 
+    /**
+     * The question wording as a simple {@code String} object.
+     */
     @Column(name = "question", nullable = false)
     private String questionWording;
+
+    /**
+     * The predefined possible answers collection of this question.
+     */
     @ElementCollection(fetch = FetchType.EAGER)
     private Set<Answer> answers;
 
+    /**
+     * Initializes a newly created {@code Question} object, with the initialization of the fields with the given
+     * values.
+     *
+     * @param questionWording {@link #questionWording}
+     * @param answers         {@link #answers}
+     */
     public Question(String questionWording, Set<Answer> answers) {
         this.questionWording = questionWording;
         this.answers = answers;
