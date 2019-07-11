deed7c05f42cff3e68b46a494ca11566255d49ee
Adraas <pickalov.artyom@yandex.ru>
2019-07-11 11:16:26 +0400

    feat: complete main window controllers The server-side and client-side controllers of main window was completed.

--- web-application/src/main/webapp/js/medical-calculator.js
+++ web-application/src/main/webapp/js/medical-calculator.js
@@ -6,6 +6,10 @@ class MedicalCalculator {
         this.xmlHttp.onload = function () {
             let response = MedicalCalculator.xmlHttp.responseText;
             if (response != null && response.trim() !== "") {
+                let container = document.getElementById(parentNodeId);
+                while (container.firstChild) {
+                    container.removeChild(container.firstChild);
+                }
                 let parser = new DOMParser();
                 let elementDOM = parser.parseFromString(response, "text/html");
                 document.getElementById(parentNodeId).appendChild(elementDOM);
@@ -13,6 +17,25 @@ class MedicalCalculator {
         };
         this.xmlHttp.open("GET", "/questionnaires-loading", false);
         this.xmlHttp.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
-        this.xmlHttp.send("get-all-questionnaires");
+        this.xmlHttp.send("message-type=get-all-questionnaires");
+    }
+
+    static receiveQuestionnaire(parentNodeId, anchorId, questionnaireTitle) {
+        this.xmlHttp.onload = function () {
+            let response = MedicalCalculator.xmlHttp.responseText;
+            if (response != null && response.trim() !== "") {
+                let container = document.getElementById(parentNodeId);
+                while (container.firstChild) {
+                    container.removeChild(container.firstChild);
+                }
+                let parser = new DOMParser();
+                let elementDOM = parser.parseFromString(response, "text/html");
+                document.getElementById(parentNodeId).appendChild(elementDOM);
+                window.location = "#" + anchorId
+            }
+        };
+        this.xmlHttp.open("GET", "/questionnaire-loading", true);
+        this.xmlHttp.setRequestHeader("Content-Type", "text/plain; charset=UTF-8");
+        this.xmlHttp.send("message-type=get-questionnaire-data&questionnaire-title=" + questionnaireTitle);
     }
 }
