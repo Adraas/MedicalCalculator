be682110672bf562d55aa48bdd2109506fe77a11
Adraas <pickalov.artyom@yandex.ru>
2019-07-11 16:34:36 +0400

    feat: add checking to required values availability

--- medical-calculator-model/src/main/java/ru/code/open/algorithm/MedicalCalculator.java
+++ medical-calculator-model/src/main/java/ru/code/open/algorithm/MedicalCalculator.java
@@ -1,6 +1,10 @@
 package ru.code.open.algorithm;
 
+import ru.code.open.entities.Answer;
 import ru.code.open.entities.MedicalQuestionnaireType;
+import ru.code.open.entities.Question;
+import ru.code.open.entities.Questionnaire;
+import ru.code.open.exceptions.AlgorithmException;
 import ru.code.open.functions.FunctionRepository;
 
 import java.util.ArrayList;
@@ -12,16 +16,42 @@ import java.util.function.Function;
 
 public class MedicalCalculator {
 
-    public Collection<Double> calculate(String calculatorTitle, String calculatorType, Map<String, Double> answers) {
-        MedicalQuestionnaireType type = MedicalQuestionnaireType.valueOf(calculatorType);
-        switch (type) {
-            case POINTS_SUMMATION_CALCULATOR:
-                return totalScoreCalculate(answers.values());
-            case FORMULA_CALCULATOR:
-                return calculateByFormulas(calculatorTitle, answers);
-            default:
-                return null;
+    public Collection<Double> calculate(Questionnaire questionnaire, Map<String, Double> answers)
+            throws AlgorithmException {
+        String calculatorTitle = questionnaire.getTitle();
+        MedicalQuestionnaireType type = questionnaire.getMedicalQuestionnaireType();
+        if (hasAllRequiredAnswers(questionnaire.getQuestions(), answers.size(), 0)) {
+            switch (type) {
+                case POINTS_SUMMATION_CALCULATOR:
+                    return totalScoreCalculate(answers.values());
+                case FORMULA_CALCULATOR:
+                    return calculateByFormulas(calculatorTitle, answers);
+                default:
+                    return null;
+            }
+        } else {
+            throw new AlgorithmException("not all required values");
         }
+    }
+
+    private boolean hasAllRequiredAnswers(Set<Question> questions, int numberAnswers, int currentValue) {
+        for (Question question : questions) {
+            if (question.getAnswers() != null) {
+                for (Answer answer : question.getAnswers()) {
+                    currentValue++;
+                    if (answer.getState() != null) {
+                        if (hasAllRequiredAnswers(answer.getState().getQuestions(), numberAnswers, currentValue)) {
+                            return true;
+                        }
+                    } else {
+                        return numberAnswers == currentValue;
+                    }
+                }
+            } else {
+                currentValue++;
+            }
+        }
+        return numberAnswers == currentValue;
     }
 
     private Collection<Double> totalScoreCalculate(Collection<Double> answers) {
