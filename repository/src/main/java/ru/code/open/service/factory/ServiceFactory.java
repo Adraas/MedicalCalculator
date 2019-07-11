41c2fcadf81a5313b030dac04884a93a83f9ae27
Adraas <pickalov.artyom@yandex.ru>
2019-07-11 13:12:56 +0400

    fix: add missing parameters

--- repository/src/main/java/ru/code/open/service/factory/ServiceFactory.java
+++ repository/src/main/java/ru/code/open/service/factory/ServiceFactory.java
@@ -11,12 +11,13 @@ import ru.code.open.service.QuestionnaireService;
 
 public class ServiceFactory implements IServiceFactory {
 
+    @SuppressWarnings(value = {"unchecked"})
     @Override
-    public IService createService(IDao iDao) {
-        Class entityClass = iDao.getClass();
-        return entityClass.equals(Patient.class) ? new PatientService()
-                : entityClass.equals(Questionnaire.class) ? new QuestionnaireService()
-                : entityClass.equals(PatientCondition.class) ? new PatientConditionService()
+    public IService createService(IDao dao) {
+        Class entityClass = dao.getClass();
+        return entityClass.equals(Patient.class) ? new PatientService(dao)
+                : entityClass.equals(Questionnaire.class) ? new QuestionnaireService(dao)
+                : entityClass.equals(PatientCondition.class) ? new PatientConditionService(dao)
                 : null;
     }
 }
